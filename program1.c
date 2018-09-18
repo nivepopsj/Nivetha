ff189a99cd276a65392a338db7a983e5f629febc
nivepopsj <43338258+nivepopsj@users.noreply.github.com>
2018-09-18 15:27:25 +0530

    Update and rename README.md to program1.c

---
+++ program1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main( )
+{
+int a,n;
+printf("\n enter the number:");
+scanf("%d",&n);
+if(a==0)
+{
+printf("zero");
+}
+else if(a<0)
+{
+printf("\n negative");
+}
+else if(a>0)
+{
+printf("\n positive");
+}
+getch( );
+}
